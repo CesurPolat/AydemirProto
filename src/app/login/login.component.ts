1c7f2dcaf1072b15c2893d178141de92916431bd
Cesur Polat <cesurpolat12@gmail.com>
2021-07-01 02:49:49 +0300

    async

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -16,25 +16,34 @@ export class LoginComponent implements OnInit {
 
 
   ngOnInit(): void {
-    this.http.get('https://jsonplaceholder.typicode.com/photos').subscribe((resp:any)=>{
-      console.log('111 - fotolar' , resp)
+    /* this.http.get('https://jsonplaceholder.typicode.com/photos').subscribe((resp: any) => {
+      console.log('111 - fotolar', resp)
     })
 
-    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((resp:any)=>{
-      console.log('222 - users' , resp)
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((resp: any) => {
+      console.log('222 - users', resp)
     })
 
-    this.http.get('https://api.aydemir.ndepo.com/api/WorkOrders/getall').subscribe((resp:any)=>{
-      console.log('333 - işler' , resp)
-    })
+    this.http.get('https://api.aydemir.ndepo.com/api/WorkOrders/getall').subscribe((resp: any) => {
+      console.log('333 - işler', resp)
+    }) */
+
+
+    this.getResult('https://jsonplaceholder.typicode.com/photos');
+    this.getResult('https://jsonplaceholder.typicode.com/users');
+    
   }
 
+  result:any;
 
-
-
-
-
-
+  async getResult(url:any): Promise<any> {
+    this.result = await this.http.get(url)
+      .toPromise()
+      .then(resp => {
+        console.log(resp);
+      });//Do you own cast here
+    return this.result;
+  }
 
 
 
